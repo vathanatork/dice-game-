2661a31700fa710fdd7e2866f313f94c6b5438f4
vathana <vath2503@gmail.com>
2023-01-04 17:37:48 +0700

    second commit

--- index.js
+++ index.js
@@ -20,28 +20,7 @@ else h1.innerHTML = "Player2 Win";
 
 
 function replaceDice(player1,player2){
-    switch(player1){
-        case 1:img1.setAttribute("src","/images/dice1.png");
-        break;
-        case 2:img1.setAttribute('src','/images/dice2.png');
-        break;
-        case 3:img1.setAttribute('src','/images/dice3.png');
-        break;
-        case 4:img1.setAttribute('src','/images/dice4.png');
-        break;
-        case 5:img1.setAttribute('src','/images/dice5.png');
-        break;
-        case 6:img1.setAttribute('src','/images/dice6.png');
-        break;
-    };
-    switch(player2){
-        case 2:img2.setAttribute('src','/images/dice2.png');break;
-        case 2:img2.setAttribute('src','/images/dice2.png');break;
-        case 1:img2.setAttribute('src','/images/dice1.png');break;
-        case 3:img2.setAttribute('src','/images/dice3.png');break;
-        case 4:img2.setAttribute('src','/images/dice4.png');break;
-        case 5:img2.setAttribute('src','/images/dice5.png');break;
-        case 6:img2.setAttribute('src','/images/dice6.png');break;
-    };
+        img1.setAttribute("src","/images/dice"+player1+".png");
+        img2.setAttribute("src","/images/dice"+player2+ ".png");
 }
 replaceDice(player1,player2);
